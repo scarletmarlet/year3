0cd97a2841e723ae5780309edf86fd6d83a6fd2e
mariaigeorgieva <mariagoergieva1009@gmail.com>
2016-10-03 16:02:17 +0100

    2nd

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
         <p>
             <?php
                 echo "Hello World";
-               echo "Hello," . " " . "world" . "!";
+                echo "Hello," . " " . "world" . "!";
                 echo 5 * 7;
             ?>
         </p>
