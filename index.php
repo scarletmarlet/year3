1c234b45aedda0f20bbaaf23699b9937b9b86639
mariaigeorgieva <mariagoergieva1009@gmail.com>
2016-10-03 15:32:26 +0100

    commands

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
         <p>
             <?php
                 echo "Hello World";
+                echo "Hello," . " " . "world" . "!";
+                echo 5 * 7;
             ?>
         </p>
     </body>
